5694402ec244637726d7dd71d3368278a143c95f
headless houdini <headlesshoudini@protonmail.com>
2022-12-19 01:54:08 +0000

    Users can add images to part description.

--- src/meta_requests.rs
+++ src/meta_requests.rs
@@ -1,75 +1,126 @@
-use std::error::Error;
-use ureq::Response;
-use crate::structs::ListChoice;
+use crate::structs::{ListChoice, MediaData};
 use crate::tools::{get_brand_models, get_makes};
+use image::{DynamicImage, ImageFormat, ImageResult};
+use std::error::Error;
+use std::process::Command;
+use ureq::Response;
 
-pub(crate) fn service_message(phone_number: &str) -> Result<(), Box<dyn Error>>{
-
-
+pub(crate) fn service_message(phone_number: &str) -> Result<(), Box<dyn Error>> {
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization",  format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
         .send_json(ureq::json!({
-    "messaging_product": "whatsapp",
-    "recipient_type": "individual",
-    "to": phone_number,
-    "type": "template",
-    "template": {
-        "name": "solicitud_vin_1",
-    "language": {
-            "code": "es"
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phone_number,
+        "type": "template",
+        "template": {
+            "name": "solicitud_vin_1",
+        "language": {
+                "code": "es"
+            }
         }
-    }
-    }))?.into_string().unwrap();
+        }))?
+        .into_string()
+        .unwrap();
     Ok(())
 }
 
-pub(crate) fn help_request(phone_number: &str) -> Result<(), Box<dyn Error>>{
+pub(crate) fn get_media_url(media_id: &str) -> Result<MediaData, Box<dyn Error>> {
+    let resp: String = ureq::get(format!("https://graph.facebook.com/v15.0/{}", media_id).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
+        .call()?
+        .into_string()
+        .unwrap();
 
+    let media_data: MediaData = serde_json::from_str(resp.as_str()).unwrap();
+    Ok(media_data)
+}
 
-    let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization",  format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
-        .send_json(ureq::json!({
-    "messaging_product": "whatsapp",
-    "recipient_type": "individual",
-    "to": phone_number,
-    "type": "template",
-    "template": {
-        "name": "solicitud_ayuda",
-    "language": {
-            "code": "es"
-        }
-    }
-    }))?.into_string().unwrap();
+pub(crate) fn get_image(image_url: &str) -> Result<(), Box<dyn Error>> {
+    // let resp = ureq::get("https://lookaside.fbsbx.com/whatsapp_business/attachments/?mid=924155428582924&ext=1671147070&hash=ATvuxxCAl4FdoCpJuo-weK8GVXgr7jMH1I1x3lMGT9N1Sg")
+    //     .set(
+    //         "Authorization",
+    //         format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+    //     ).call().unwrap();
+
+    let output = Command::new("curl")
+        .arg("-X GET")
+        .arg(format!("'{}'", "https://lookaside.fbsbx.com/whatsapp_business/attachments/?mid=924155428582924&ext=1671147430&hash=ATt2lYiLFQdJRXSw7q_Q6Jy6vEPJXb2L3Tsa9-5vlRDL7w"))
+        .arg(format!("-H 'Authorization: Bearer {}'",std::env::var("META_TOKEN").unwrap()).as_str())
+        .arg("> media_file.jpeg.jpeg")
+        .output()
+        .expect("Failed to execute command");
+
     Ok(())
 }
 
-pub(crate) fn request_vin(phone_number: &str) -> Result<(), Box<dyn Error>>{
-
+pub(crate) fn help_request(phone_number: &str) -> Result<(), Box<dyn Error>> {
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
         .send_json(ureq::json!({
-    "messaging_product": "whatsapp",
-    "recipient_type": "individual",
-    "to": phone_number,
-    "type": "template",
-    "template": {
-        "name": "solicitud_vin_3",
-    "language": {
-            "code": "es"
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phone_number,
+        "type": "template",
+        "template": {
+            "name": "solicitud_ayuda",
+        "language": {
+                "code": "es"
+            }
         }
-    }
-    }))?
+        }))?
+        .into_string()
+        .unwrap();
+    Ok(())
+}
+
+pub(crate) fn request_vin(phone_number: &str) -> Result<(), Box<dyn Error>> {
+    let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
+        .send_json(ureq::json!({
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phone_number,
+        "type": "template",
+        "template": {
+            "name": "solicitud_vin_3",
+        "language": {
+                "code": "es"
+            }
+        }
+        }))?
         .into_string()?;
 
     Ok(())
 }
 
-pub(crate) fn model_list(phone_number: &str, brand_selected: String, page:i32) -> Result<(), Box<dyn Error>>{
-
-
-
-    let models_list = get_brand_models(brand_selected.to_ascii_lowercase().as_str(),page, brand_selected.as_str());
-    let modal_title = format!("Modelos {} - {}", brand_selected[0..1].to_uppercase() + &brand_selected[1..] ,page);
+pub(crate) fn model_list(
+    phone_number: &str,
+    brand_selected: String,
+    page: i32,
+) -> Result<(), Box<dyn Error>> {
+    let models_list = get_brand_models(
+        brand_selected.to_ascii_lowercase().as_str(),
+        page,
+        brand_selected.as_str(),
+    );
+    let modal_title = format!(
+        "Modelos {} - {}",
+        brand_selected[0..1].to_uppercase() + &brand_selected[1..],
+        page
+    );
 
     if models_list.is_err() {
         panic!("Error obtaining model list");
@@ -77,42 +128,41 @@ pub(crate) fn model_list(phone_number: &str, brand_selected: String, page:i32) -
 
     let model_json = serde_json::to_string(&models_list.unwrap()).unwrap();
 
-    println!("{}", model_json);
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
         .send_json(ureq::json!({
-    "recipient_type": "individual",
-    "to": phone_number,
-    "messaging_product": "whatsapp",
-    "type": "interactive",
-    "interactive": {
-        "type": "list",
-    "header": {
-            "type": "text",
-    "text": "Busqueda de repuesto"
-        },
-    "body": {
-            "text": "Seleccione modelo de auto."
-        },
-    "action": {
-            "button": "Modelos",
-    "sections": [
-            {
-                "title": modal_title,
-            "rows": model_json
+        "recipient_type": "individual",
+        "to": phone_number,
+        "messaging_product": "whatsapp",
+        "type": "interactive",
+        "interactive": {
+            "type": "list",
+        "header": {
+                "type": "text",
+        "text": "Busqueda de repuesto"
+            },
+        "body": {
+                "text": "Seleccione modelo de auto."
+            },
+        "action": {
+                "button": "Modelos",
+        "sections": [
+                {
+                    "title": modal_title,
+                "rows": model_json
+                }
+                        ]
             }
-                    ]
         }
-    }
-    }))?
+        }))?
         .into_string()?;
-
-    println!("response {}", resp);
 
     Ok(())
 }
-pub(crate) fn brand_list(phone_number: &str, page:i32) -> Result<(), Box<dyn Error>>{
-
+pub(crate) fn brand_list(phone_number: &str, page: i32) -> Result<(), Box<dyn Error>> {
     let make_list = get_makes(page);
     let modal_title = format!("Marcas - pagina {}", page);
 
@@ -123,62 +173,64 @@ pub(crate) fn brand_list(phone_number: &str, page:i32) -> Result<(), Box<dyn Err
     let make_json = serde_json::to_string(&make_list.unwrap()).unwrap();
 
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
         .send_json(ureq::json!({
-    "recipient_type": "individual",
-    "to": phone_number,
-    "messaging_product": "whatsapp",
-    "type": "interactive",
-    "interactive": {
-        "type": "list",
-    "header": {
-            "type": "text",
-    "text": "Busqueda de repuesto"
-        },
-    "body": {
-            "text": "Seleccione marca de auto."
-        },
-    "action": {
-            "button": modal_title,
-    "sections": [
-    {
-        "title": "Marca",
-    "rows": make_json
-    }
-    ]
+        "recipient_type": "individual",
+        "to": phone_number,
+        "messaging_product": "whatsapp",
+        "type": "interactive",
+        "interactive": {
+            "type": "list",
+        "header": {
+                "type": "text",
+        "text": "Busqueda de repuesto"
+            },
+        "body": {
+                "text": "Seleccione marca de auto."
+            },
+        "action": {
+                "button": modal_title,
+        "sections": [
+        {
+            "title": "Marca",
+        "rows": make_json
         }
-    }
-    }))?
+        ]
+            }
+        }
+        }))?
         .into_string()?;
 
     Ok(())
 }
 
 fn execute_second_step(phone_number: &str) -> Result<(), Box<dyn Error>> {
-
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
         .send_json(ureq::json!({
-    "messaging_product": "whatsapp",
-    "recipient_type": "individual",
-    "to": phone_number,
-    "type": "template",
-    "template": {
-        "name": "solicitud_vin_2",
-    "language": {
-            "code": "es"
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": phone_number,
+        "type": "template",
+        "template": {
+            "name": "solicitud_vin_2",
+        "language": {
+                "code": "es"
+            }
         }
-    }
-    }))?
+        }))?
         .into_string()?;
-
-    println!("{}", &resp);
 
     Ok(())
 }
 
 pub(crate) fn successfull_request(phone_number: &str) -> Result<(), Box<dyn Error>> {
-
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
         .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
         .send_json(ureq::json!(
@@ -197,9 +249,7 @@ pub(crate) fn successfull_request(phone_number: &str) -> Result<(), Box<dyn Erro
     Ok(())
 }
 
-
 pub(crate) fn request_part_description(phone_number: &str) -> Result<(), Box<dyn Error>> {
-
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
         .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
         .send_json(ureq::json!(
@@ -210,55 +260,57 @@ pub(crate) fn request_part_description(phone_number: &str) -> Result<(), Box<dyn
                 "to": phone_number,
                 "type": "text",
                 "text": {
-                    "body": "Describa el repuesto que necesita, provea la mayor cantidad de detalles que permitan distinguir el repuesto."
+                    "body": "Describa el repuesto que necesita, provea la mayor cantidad de detalles que permitan distinguir el repuesto, puedes adjuntar una imagen en el mismo mensaje."
                 }
             }))?
         .into_string()?;
-
-    println!("{}", &resp);
 
     Ok(())
 }
 
 pub(crate) fn new_request(phone_number: &str) -> Result<(), Box<dyn Error>> {
-
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
         .send_json(ureq::json!(
-            {
-                "preview_url": false,
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": phone_number,
-                "type": "text",
-                "text": {
-                    "body": "Si desea"
-                }
-            }))?
+        {
+            "preview_url": false,
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": phone_number,
+            "type": "text",
+            "text": {
+                "body": "Si desea"
+            }
+        }))?
         .into_string()?;
-
-    println!("{}", &resp);
 
     Ok(())
 }
 
-pub fn send_error_message(error_message: impl Into<String>, phone_number: &str) -> Result<(), Box<dyn Error>>{
+pub fn send_error_message(
+    error_message: impl Into<String>,
+    phone_number: &str,
+) -> Result<(), Box<dyn Error>> {
     let resp: String = ureq::post("https://graph.facebook.com/v15.0/109252135350289/messages")
-        .set("Authorization", format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str())
+        .set(
+            "Authorization",
+            format!("Bearer {}", std::env::var("META_TOKEN").unwrap()).as_str(),
+        )
         .send_json(ureq::json!(
-            {
-                "preview_url": false,
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": phone_number,
-                "type": "text",
-                "text": {
-                    "body": error_message.into()
-                }
-            }))?
+        {
+            "preview_url": false,
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": phone_number,
+            "type": "text",
+            "text": {
+                "body": error_message.into()
+            }
+        }))?
         .into_string()?;
-
-    println!("{}", &resp);
 
     Ok(())
 }
